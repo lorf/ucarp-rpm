e81c43a922ede39c3eb494bba69d0aea551d4c1e
Dmitry Frolov <dmitry.frolov@gmail.com>
2019-06-19 00:22:05 +0700

    Build against https://github.com/lorf/UCarp

--- Readme.md
+++ Readme.md
@@ -1,8 +1,7 @@
 This repository provides a script for building a ucarp RPM for RHEL, or
-derivative, systems. This RPM is different from the EPEL package in that it
-is built against the [pidfile branch][pbranch] of the upstream source.
+derivative, systems. This RPM is different from the EPEL package in that it is
+built against the https://github.com/lorf/UCarp repo containing UDP broadcast
+options.
 
 Other than that, the RPM specific resources have been borrowed from the
-EPEL source -- http://pkgs.fedoraproject.org/cgit/ucarp.git/
-
-[pbranch]: https://github.com/jsumners/UCarp/tree/pidfile
+EPEL source -- https://src.fedoraproject.org/rpms/ucarp
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ CWD=$(pwd)
 VERSION="1.5.2"
 SPEC="files/ucarp.spec"
 #SOURCE_URL="https://github.com/jedisct1/UCarp.git"
-SOURCE_URL="https://github.com/contix/UCarp.git"
+SOURCE_URL="https://github.com/lorf/UCarp.git"
 
 echo "Creating RPM build path structure..."
 mkdir -p rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS,tmp}
@@ -21,6 +21,7 @@ rm -f "ucarp-$VERSION.tar.bz2" >/dev/null 2>&1 || true
 echo "Tarring source tree..."
 tar cjf "ucarp-$VERSION.tar.bz2" ucarp-${VERSION}
 
+gitrev=`git log -1 --format=%h`
 if [ -f ${CWD}/gpg-env ]; then
   echo "Building RPM with GPG signing..."
   cd ${CWD}
@@ -30,16 +31,18 @@ if [ -f ${CWD}/gpg-env ]; then
     rpmbuild --define "_topdir ${CWD}/rpmbuild" \
       --define "_signature ${signature}" \
       --define "_gpg_path ${gpg_path}" --define "_gpg_name ${gpg_name}" \
-      --define "__gpg ${gpg_bin}" --sign -bb ${SPEC}
+      --define "__gpg ${gpg_bin}" --define "dist git$gitrev" \
+      --sign -bb ${SPEC}
   else
     rpmbuild --define "_topdir ${CWD}/rpmbuild" \
       --define "_signature ${signature}" \
       --define "_gpg_path ${gpg_path}" --define "_gpg_name ${gpg_name}" \
-      --sign -bb ${SPEC}
+      --define "dist git$gitrev" --sign -bb ${SPEC}
   fi
 else
   echo "Building RPM..."
   cd ${CWD}
-  rpmbuild --define "_topdir ${CWD}/rpmbuild" -bb ${SPEC}
+  rpmbuild --define "_topdir ${CWD}/rpmbuild" \
+      --define "dist git$gitrev" -bb ${SPEC}
 fi
 
